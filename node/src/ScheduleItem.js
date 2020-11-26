fbf4001ee326ba39b05a043370c2bfe545ada6f6
Tony Ruan <tonyr2@illinois.edu>
2020-11-25 23:26:01 -0600

    Add unfinished schedule viewer

--- node/src/ScheduleItem.js
+++ node/src/ScheduleItem.js
@@ -1,20 +1,66 @@
 import React from 'react';
 import './ScheduleItem.css';
-import {Accordion, Card} from 'react-bootstrap'
+import {Accordion, Card, Container} from 'react-bootstrap'
 
 function ScheduleItem(props) {
 
+    let data = props.data;
+
+    if (data == undefined) {
+      data = {
+        subject: "CS",
+        number: 225,
+        instructorName: "Smith, Bob",
+        rating: 5,
+        creditHours: "3 hours.",
+        title: "Abc",
+        term: "Fall",
+        year: "2019",
+        numStudents: 150,
+        description: "dsfhskjrddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+        ACP: "ACP",
+        NAT: "",
+        CS: "WCC",
+        QR: "QR1",
+        HUM: "",
+        SBS: "",
+        avgGPA: 3.42222,
+        aPlus: 31,
+        a: 12,
+        aMinus: 14,
+        bPlus: 12,
+        b: 6,
+        bMinus: 3,
+        cPlus: 9,
+        c: 2,
+        cMinus: 8,
+        dPlus: 2,
+        d: 1,
+        dMinus: 0,
+        f: 0,
+        w: 1
+      };
+    }
+
     return (
-      <Accordion defaultActiveKey="0">
-        <Card>
-          <Accordion.Toggle as={Card.Header} eventKey="0">
-            Click me!
-          </Accordion.Toggle>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>Hello! I'm the body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion>
+      <Container>
+
+        <Accordion defaultActiveKey="0">
+          <Card className="schedule-item">
+
+            <Accordion.Toggle className="schedule-item-header" as={Card.Header} eventKey="1">
+              {data.subject + " " + String(data.number) + " (" + data.instructorName + ")"}
+            </Accordion.Toggle>
+
+            <Accordion.Collapse eventKey="1">
+              <Card.Body>{data.description}</Card.Body>
+            </Accordion.Collapse>
+
+          </Card>
+        </Accordion>
+
+      </Container>
+
     );
 }
 
--- node/src/ScheduleViewer.js
+++ node/src/ScheduleViewer.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ScheduleViewer.css';
 import ScheduleItem from './ScheduleItem';
+import {Container} from 'react-bootstrap'
+import axios from 'axios'
+
 import {
-  useParams
+  useParams, useHistory
 } from "react-router-dom";
 
 function ScheduleViewer() {
   let { scheduleId } = useParams();
+  const url = window.location.href;
+
+  const [data, setData] = useState(undefined);
+
+  const api = axios.create({
+    baseURL: `https://reqres.in/api`
+  })
+
+  api.get("/users/2")
+      .then(res => {
+          setData(res.data.data)
+       })
+       .catch(error=>{
+           console.log("Error")
+       });
 
   return (
-    <ScheduleItem>
-    </ScheduleItem>
+    <Container className="schedule-viewer">
+      <h3 className="text-center">{url}</h3>
+      <ScheduleItem></ScheduleItem>
+
+      <ScheduleItem></ScheduleItem>
+    </Container>
   );
 }
 
